8fda26c9610e0b6c8b73f4fe5aa7784d09db4f6a
BassThatHertz <48166845+BassThatHertz@users.noreply.github.com>
2019-05-26 18:18:17 +0100

    Update resampler.py

--- resampler.py
+++ resampler.py
@@ -2,13 +2,18 @@ import os
 
 current_dir_files = os.listdir()
 
-filepath_of_sox = os.path.join("sox", "sox")
-
 compatible_filetypes = ["wav"]
 compatible_file_list = [filename for filename in current_dir_files if filename.split(".")[-1] in compatible_filetypes]
 
+filepath_of_sox = os.path.join("sox", "sox")
+
+desired_sample_rate = int(input("What would you like to change the sample rate to? \nUnit: Hz \nExample: For 44.1kHz, enter '44100'\nPlease enter an integer: "))
+
+sample_rate_in_kHz = desired_sample_rate / 1000
+sample_rate_in_kHz = str(sample_rate_in_kHz)
+end_of_filename = " [{}kHz]".format(sample_rate_in_kHz)
+print(end_of_filename)
 
 for file in compatible_file_list:
     print("Resampling {}...".format(file))
-    os.system('{} -G "{}" -r 44100 "{}".wav'.format(filepath_of_sox, file, file + " [44.1kHz]"))
-
+    os.system('{} -G "{}" -r {} "{}".wav'.format(filepath_of_sox, file, desired_sample_rate, file + end_of_filename))
