6a0ac71b7b14691532c0b42c29e9613d330be5e0
BassThatHertz <48166845+BassThatHertz@users.noreply.github.com>
2020-07-13 17:34:54 +0100

    Update resampler.py

--- resampler.py
+++ resampler.py
@@ -1,21 +1,27 @@
 import os 
 
-current_dir_files = os.listdir()
-
-compatible_filetypes = ["wav"]
-compatible_file_list = [filename for filename in current_dir_files if filename.split(".")[-1] in compatible_filetypes]
+wav_files = [filename for filename in os.listdir() if os.path.splitext(filename)[-1] == '.wav']
 
 filepath_of_sox = os.path.join("sox", "sox")
 
-desired_sample_rate = int(input("What would you like to change the sample rate to? \nUnit: Hz \nExample: For 44.1kHz, enter '44100'\nPlease enter an integer: "))
+print('You can choose one of the following sample rates:')
 
-sample_rate_in_kHz = desired_sample_rate / 1000
-end_of_filename = " [{}kHz]".format(sample_rate_in_kHz)
+sample_rate_options = ['44.1 kHz', '48 kHz', '96 kHz', '192 kHz']
 
-for file in compatible_file_list:
-    new_filename = file[:-4] + end_of_filename + ".wav"
+for position, sample_rate in enumerate(sample_rate_options):
+	print(position, sample_rate) 
+
+sample_rate_index = int(input('Choose your desired sample rate by entering the number that it is associated with.\n'
+	'For example, to choose 44.1 kHz, enter 0: '))
+
+sample_rate = int(sample_rate_options[sample_rate_index][:-4]) * 1000
+
+end_of_filename = ' [' + sample_rate_options[sample_rate_index] + ']'
+
+for file in wav_files:
+    new_filename = os.path.splitext(file)[0] + end_of_filename + ".wav"
     print("Resampling {}...".format(file))
     os.system('{} -G "{}" -r {} "{}"'.format(filepath_of_sox, file, desired_sample_rate, new_filename))
-    print("Resample complete! Resampled file saved as {}".format(new_filename))
+    print("{} resampled. Resampled file saved as {}".format(file, new_filename))
 
-prevent_window_from_closing = input("You may now close this window.")
+input("You may now close this window.")
